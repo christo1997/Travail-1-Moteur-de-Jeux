2df4c4e3e38660e31a552b0c85a209daf39207cd
Christopher Imbeault <christo.1997@hotmail.com>
2019-10-08 12:07:20 -0400

    Création des scripts

---
+++ Assets/Scripts/ClassementScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClassementScript : MonoBehaviour
+{
+    public GameObject position;
+    public GameObject score;
+    public GameObject nom;
+
+    public void SetScore(string position, string score, string nom)
+    {
+        this.position.GetComponent<Text>().text = position;
+        this.score.GetComponent<Text>().text = score;
+        this.nom.GetComponent<Text>().text = nom;
+    }
+} 
+
